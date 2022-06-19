0325edb83b881b68ca44aa58b81f651d4119e99d
Alfian Firmansyah <105913725+gnomefin@users.noreply.github.com>
2022-06-19 12:12:53 +0700

    upload asset and db backup scheduler

---
+++ db-backup-scheduler.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+# please run this as cron job root.
+
+# set path for aws 
+export PATH=$PATH:/snap/bin
+
+dir="/tmp/dnmgnome-db-backup"
+
+if [[ ! -e $dir ]]; then
+    mkdir -p $dir
+elif [[ ! -d $dir ]]; then
+    echo "$dir already exists but is not a directory" 1>&2
+fi
+
+cd $dir
+
+# Change timezone to jakarta WIB
+export TZ=Asia/Jakarta
+date=$(date '+%Y-%m-%d_%H-%M-%S_%Z')
+
+# DB Creds
+DB_HOSTNAME=dn-db.gnome-hub.internal
+DB_USERNAME=gnome
+DB_PASSWORD=Capcapcap123
+
+# Backupdb
+mysqldump -h $DB_HOSTNAME -u $DB_USERNAME -p$DB_PASSWORD --all-databases > db-$date.sql
+
+# upload to s3
+aws s3 cp db-$date.sql s3://dnmgnome-db-backup
+
+# remove 1 old file after success, we will retain atleast 1.
+ls -t | sed -e '1,1d' | xargs -d '\n' -r rm
