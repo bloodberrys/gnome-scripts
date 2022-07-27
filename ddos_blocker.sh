1083fae4e38dc6df0ad80ac11ae7c04a166cea82
Alfian Firmansyah <105913725+gnomefin@users.noreply.github.com>
2022-07-27 09:47:47 +0700

    save

---
+++ ddos_blocker.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+netstat -npt | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr > /tmp/tcp_by_ip_count.log
+netstat -npt | grep 14711 | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr > /tmp/tcp_14711.log
