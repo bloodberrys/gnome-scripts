43b049ed181976dc4fc225b018ec645cbcc78f23
gnomefin <alfianvansykes@gmail.com>
2022-07-29 05:24:23 +0700

    all testing done

--- ddos_blocker.sh
+++ ddos_blocker.sh
@@ -1,4 +1,88 @@
 #!/bin/bash
 
-netstat -npt | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr > /tmp/tcp_by_ip_count.log
-netstat -npt | grep 14711 | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr > /tmp/tcp_14711.log
+run_process(){
+    local _filename=$1
+    local _timestamp=$2
+    array_of_ip_count=$(cat /tmp/${_filename}.log | grep -Eo '^[0-9]+' | tr '\n' ' ')
+    array_of_ip=$(cat /tmp/${_filename}.log | grep -Eo '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$' | tr '\n' ' ')
+
+    IFS=" " read -r -a ipcounts <<< "$array_of_ip_count"
+    IFS=" " read -r -a ips <<< "$array_of_ip"
+
+    iplength=${#ips[@]}
+    counter=0
+    for((i=0; i<iplength; i++))
+    do  
+        # check the ip count that more than 10
+        if [ ${ipcounts[$i]} -gt 10 ]; then
+            # save the ip to list with command
+            echo "iptables -A INPUT -s ${ips[$i]} -j DROP" >> "/tmp/ip_blocked_${_filename}.log"
+            echo "${ips[$i]}" >> "/tmp/iplist_${_filename}.log"
+            
+            # checking the ips
+            sudo iptables -C INPUT -s ${ips[$i]} -j DROP
+
+            # If the exit status is 1, meaning we need to execute the ips to ip tables
+            if [ $? -eq 1 ]; then
+                # execute the iptables block
+                sudo iptables -A INPUT -s ${ips[$i]} -j DROP
+                counter=$((counter+1))
+            fi
+        fi
+    done
+
+    if [ $counter -gt 0 ]; then
+        message=$(awk '{printf "%s<br>", $0}' "/tmp/iplist_${_filename}.log")
+        string="There are $counter IPs blocked:<br>$message"
+        filename="/tmp/ip_blocked_${_filename}.log"
+        iplistfile="/tmp/${_filename}.log"
+        send_discord_security_report "$string" "$filename" "$iplistfile" "$_timestamp"
+    else
+        echo -e "ALL GOOD, ALL OK and nothing to do."
+    fi
+
+    counter=0
+
+}
+
+send_discord_security_report(){
+    webhook_url=https://discord.com/api/webhooks/1002015434532466779/UqyXpNSrj28Jop_77beQeSJ2tn9nd-I-vlMM1GzHPeFRklu-Sdw--tNXbtmRsu5u67bu
+    local _message=$1
+    local _filename=$2
+    local _iplistfile=$3
+    local _timestamp=$4
+    SUBJECT="⚠️ Security Report - $_timestamp ⚠️"
+    CONTENT=$(echo $_message | sed 's3<br>3\n3g')
+    payload_json=$(jq -n --arg content "$CONTENT" --arg subject "$SUBJECT" '{username: "Gnome-Security", content: "\( $subject )\n\n\( $content )"}')
+    curl -g -F "payload_json=$payload_json" -F "file1=@$_filename" -F "file2=@$_iplistfile" "$webhook_url"
+}
+
+export TZ=Asia/Jakarta
+timestamp=$(date '+%Y-%m-%d_%H-%M-%S_%Z')
+
+echo -e "[TASK] Checking the netstat..."
+netstat -npt | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr | grep -Eo '[0-9]+[ ]{1}[0-9.]+' | grep -vE '^[0-9]+[ ][1][0].[0].[0-9]{1,3}.[0-9]{1,3}$' | grep -vE '^[0-9]+[ ]127.0.0.1$' > "/tmp/ip_list_total_$timestamp.log"
+netstat -npt | grep 14711 | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr | grep -Eo '[0-9]+[ ]{1}[0-9.]+' | grep -vE '^[0-9]+[ ][1][0].[0].[0-9]{1,3}.[0-9]{1,3}$' | grep -vE '^[0-9]+[ ]127.0.0.1$' > "/tmp/ip_14711_$timestamp.log"
+
+echo -e "\n[PRE-TASK] Obtaining and processing the netstat result file from /tmp/ip_list_total.log and /tmp/ip_14711.log"
+ipfiles="/tmp/ip_list_total_$timestamp.log" # WE CAN SIMPLY CHANGE THIS FOR DEBUGGING
+if [ ! -f "$ipfiles" ]; then
+    echo "✗ File ${ipfiles} not found on your machine!"
+    echo "exitting..."
+    exit 1
+else
+    echo "✓ File ${ipfiles} is found, continue to the next task..."
+    ipfiles2="/tmp/ip_14711_$timestamp.log" # WE CAN SIMPLY CHANGE THIS FOR DEBUGGING
+    if [ ! -f "$ipfiles2" ]; then
+        echo "✗ File ${ipfiles2} not found on your machine!"
+        echo "exitting..."
+        exit 1
+    else
+        echo "✓ File ${ipfiles2} is found, continue to the next task..."
+        run_process "ip_list_total_$timestamp" "$timestamp"
+        run_process "ip_14711_$timestamp" "$timestamp"
+    fi
+fi
+
+
+
