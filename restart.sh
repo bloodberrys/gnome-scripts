8e1699e3eb875abfccb6254ad8c6e02ec49d0ead
gnomefin <105913725+gnomefin@users.noreply.github.com>
2022-09-28 02:58:49 +0700

    Update restart.sh

--- restart.sh
+++ restart.sh
@@ -2,7 +2,7 @@
 
 send_discord(){
     webhook_url=https://discord.com/api/webhooks/990288785587707934/rPLMgznwPKHxRhlnzcILJgP_YjyVQaKcS_mnaLpXU6NWsY3WaPsPg9llgFibWVXB-0j_
-    SUBJECT="!!!Server restart by link click!!!"
+    SUBJECT="!!!Server restart by link click!!! server restarting..."
     local _message='server restarting...'
     ADMIN_ROLE="<@&983558291261112370>"
     BOOSTER_ROLE="<@&983549809409552445>"
@@ -13,7 +13,7 @@ send_discord(){
         CONTENT=${CONTENT:0:1500}
     fi
     payload_json=$(jq -n --arg content "$CONTENT" --arg subject "$SUBJECT" --arg ar "$ADMIN_ROLE" --arg br "$BOOSTER_ROLE" '{username: "Gnome-Automation", content: "\( $subject )\nCC: \( $br ) \( $ar )\n\n\( $content )"}')
-    curl -g -F "payload_json=$payload_json" -F "file1=@/tmp/discordmsg.log" "$webhook_url"
+    curl -g -F "payload_json=$payload_json" "$webhook_url"
 }
 
 ipfiles="/var/www/html/suspected_ip/restart" # WE CAN SIMPLY CHANGE THIS FOR DEBUGGING
