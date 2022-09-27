1a3cd1cde7d6099ff150b9541ad91298a10f75e8
root <root@ip-10-0-12-232.gnome-hub.internal>
2022-09-27 23:51:55 +0700

    a

--- packet_capture.sh
+++ packet_capture.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-nohup tcpdump port 24001 or port 56956 or port 25001 -w /tmp/file_result.pcap &
+/sbin/tcpdump port 24001 or port 56956 or port 25001 -w /home/ec2-user/file_result.pcap &
 
-sleep 900
+sleep 300
 pkill -9 tcpdump
-aws s3 cp /tmp/file_result.pcap s3://gnome-hub.com/
-rm -rf /tmp/file_result.pcap
+aws s3 cp /home/ec2-user/file_result.pcap s3://gnome-hub.com/
+rm -rf /home/ec2-user/file_result.pcap
