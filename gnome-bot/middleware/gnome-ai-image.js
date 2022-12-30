ffe620b86993bcba6fe31f0cddb84cce9bbb7f16
Alfian Firmansyah <105913725+gnomefin@users.noreply.github.com>
2022-12-30 20:07:30 +0700

    a

--- gnome-bot/middleware/gnome-ai-image.js
+++ gnome-bot/middleware/gnome-ai-image.js
@@ -8,7 +8,12 @@ import {
     OpenAIApi
 } from 'openai';
 
-async function imageChatGPT(prompt = 'ducks') {
+import {
+    readFileSync,
+    createReadStream
+} from 'fs';
+
+async function imageChatGPT(prompt = 'porn') {
 
     const configuration = new Configuration({
         apiKey: process.env.OPENAI_API_KEY,
@@ -34,5 +39,36 @@ async function imageChatGPT(prompt = 'ducks') {
         }
     }
 }
+async function imageEditChatGPT(prompt = 'A person with christmas hat', raw_image = '/Users/alfian.firmansyah/Downloads/GnomeArt/CrusaderDragon3.png', mask = '/Users/alfian.firmansyah/Downloads/Santa-Claus-Hat-PNG-Images.png') {
 
-imageChatGPT()
+    const configuration = new Configuration({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+    const openai = new OpenAIApi(configuration);
+
+    console.log(createReadStream(mask))
+    console.log(raw_image)
+
+    try {
+        const image = await openai.createImageEdit({
+            image: createReadStream(raw_image),
+            mask: createReadStream(mask),
+            prompt: String(prompt),
+            n: 1,
+            size: "1024x1024"
+        });
+        console.log(image.data.data[0].url)
+        return image.data.data[0].url;
+    } catch (error) {
+        if (error.response) {
+            console.log(error.response.status);
+            console.log(error.response.data);
+            return error.response.status + ', ' + error.response.data;
+        } else {
+            console.log(error.message);
+            return error.message
+        }
+    }
+}
+
+imageEditChatGPT()
