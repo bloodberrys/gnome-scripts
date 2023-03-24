a0480d82bbc1e94ddd8e966204c8d9c0fe57ddde
Alfian Firmansyah <alfianvansykes@gmail.com>
2023-03-25 05:54:10 +0700

    new apk asset

--- auto-import/auto-import.py
+++ auto-import/auto-import.py
@@ -2,20 +2,20 @@ import pyautogui
 import os
 import time
 import xml.etree.ElementTree as ET
-import tensorflow as tf
+# import tensorflow as tf
 
-device_name = tf.test.gpu_device_name()
-if device_name != '/device:GPU:0':
-    raise SystemError('GPU device not found')
-print('Found GPU at: {}'.format(device_name))
+# device_name = tf.test.gpu_device_name()
+# if device_name != '/device:GPU:0':
+#     raise SystemError('GPU device not found')
+# print('Found GPU at: {}'.format(device_name))
 
-device_name = tf.test.gpu_device_name()
-if device_name != '/device:GPU:0':
-    print(
-        '\n\nThis error most likely means that this notebook is not '
-        'configured to use a GPU.  Change this in Notebook Settings via the '
-        'command palette (cmd/ctrl-shift-P) or the Edit menu.\n\n')
-    raise SystemError('GPU device not found')
+# device_name = tf.test.gpu_device_name()
+# if device_name != '/device:GPU:0':
+#     print(
+#         '\n\nThis error most likely means that this notebook is not '
+#         'configured to use a GPU.  Change this in Notebook Settings via the '
+#         'command palette (cmd/ctrl-shift-P) or the Edit menu.\n\n')
+#     raise SystemError('GPU device not found')
 
 # os.startfile("C:\\Users\\User\\Documents\\GitHub\\gnome-scripts\\auto-import\\UABE\\64bit\\AssetBundleExtractor.exe")
 # time.sleep(2)
@@ -87,63 +87,63 @@ asset_xml = "assetslocation/apk/textassets.xml"
 #         # Your code here
 #loop over the files in the directory
 for filename in os.listdir(folder):
-left, top = click_file(clickType="double", file_path="imagelocate/UABE.png", duration=0, add_left=-10, add_top=35)
-pyautogui.moveTo(left-10, top+55, duration=0, tween=pyautogui.easeInOutQuad)
-pyautogui.click()
+    left, top = click_file(clickType="double", file_path="imagelocate/UABE.png", duration=0, add_left=-10, add_top=35)
+    pyautogui.moveTo(left-10, top+55, duration=0, tween=pyautogui.easeInOutQuad)
+    pyautogui.click()
 
-# get assetbundle from filename location mapping xml
-tree = ET.parse(asset_xml)
-# Get the root element
-root = tree.getroot()
+    # get assetbundle from filename location mapping xml
+    tree = ET.parse(asset_xml)
+    # Get the root element
+    root = tree.getroot()
 
-# Find the Asset element with the specified Name
-name_input = os.path.splitext(filename)[0]
-asset = None
-for a in root.findall('Asset'):
-    if a.find('Name').text == name_input:
-        asset = a
-        break
+    # Find the Asset element with the specified Name
+    name_input = os.path.splitext(filename)[0]
+    asset = None
+    for a in root.findall('Asset'):
+        if a.find('Name').text == name_input:
+            asset = a
+            break
 
-# Extract the Source element and print its text
-if asset is not None:
-    source = asset.find('Source').text
-else:
-    print(f"No asset found with Name '{name_input}'")
+    # Extract the Source element and print its text
+    if asset is not None:
+        source = asset.find('Source').text
+    else:
+        print(f"No asset found with Name '{name_input}'")
 
-source_filename = os.path.basename(source)
+    source_filename = os.path.basename(source)
 
-print("[GET ASSET BUNDLE]\n")
-click_file(clickType="double", file_path="imagelocate/APK_AB_DATA.png", center=True)
+    print("[GET ASSET BUNDLE]\n")
+    click_file(clickType="double", file_path="imagelocate/APK_AB_DATA.png", center=True)
 
-pyautogui.press('tab')
-pyautogui.press('tab')
-pyautogui.press('tab')
-print(f'[PROCESS] Assetbundle Name: {source_filename}\n\n')
-pyautogui.typewrite(f'{source_filename}')
-pyautogui.press('enter')
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    print(f'[PROCESS] Assetbundle Name: {source_filename}\n\n')
+    pyautogui.typewrite(f'{source_filename}')
+    pyautogui.press('enter')
 
-click_file(clickType="double", file_path="imagelocate/TEXTAS.png",add_left=-290, add_top=10)
-click_file(clickType="double", file_path="imagelocate/PLUGIN.png", center=True)
-click_file(clickType="double", file_path="imagelocate/IMPORT_TXT.png", center=True)
-click_file(clickType="double", file_path="imagelocate/ASSET_FILES.png", center=True)
+    click_file(clickType="double", file_path="imagelocate/TEXTAS.png",add_left=-290, add_top=10)
+    click_file(clickType="double", file_path="imagelocate/PLUGIN.png", center=True)
+    click_file(clickType="double", file_path="imagelocate/IMPORT_TXT.png", center=True)
+    click_file(clickType="double", file_path="imagelocate/ASSET_FILES.png", center=True)
 
-print("[IMPORT] Importing asset file\n\n")
-pyautogui.press('tab')
-pyautogui.press('tab')
-pyautogui.press('tab')
-print(filename)
-pyautogui.typewrite(f'{filename}')
-pyautogui.press('enter')
+    print("[IMPORT] Importing asset file\n\n")
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    print(filename)
+    pyautogui.typewrite(f'{filename}')
+    pyautogui.press('enter')
 
-click_file(clickType="double", file_path="imagelocate/OK.png", center=True)
-pyautogui.press('enter')
+    click_file(clickType="double", file_path="imagelocate/OK.png", center=True)
+    pyautogui.press('enter')
 
-print(f"[SAVE] Saving new assetbundle {filename}\n\n")
-click_file(clickType="double", file_path="imagelocate/RESULTS.png", center=True)
-pyautogui.press('tab')
-pyautogui.press('tab')
-pyautogui.press('tab')
-pyautogui.press('enter')
+    print(f"[SAVE] Saving new assetbundle {filename}\n\n")
+    click_file(clickType="double", file_path="imagelocate/RESULTS.png", center=True)
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    pyautogui.press('tab')
+    pyautogui.press('enter')
 
 
 
