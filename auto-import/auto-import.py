e6d8202d5918cd525472a6d8e63ff3c087c3ad71
Alfian Firmansyah <alfianvansykes@gmail.com>
2023-04-01 10:45:05 +0700

    a

--- auto-import/auto-import.py
+++ auto-import/auto-import.py
@@ -2,23 +2,6 @@ import pyautogui
 import os
 import time
 import xml.etree.ElementTree as ET
-# import tensorflow as tf
-
-# device_name = tf.test.gpu_device_name()
-# if device_name != '/device:GPU:0':
-#     raise SystemError('GPU device not found')
-# print('Found GPU at: {}'.format(device_name))
-
-# device_name = tf.test.gpu_device_name()
-# if device_name != '/device:GPU:0':
-#     print(
-#         '\n\nThis error most likely means that this notebook is not '
-#         'configured to use a GPU.  Change this in Notebook Settings via the '
-#         'command palette (cmd/ctrl-shift-P) or the Edit menu.\n\n')
-#     raise SystemError('GPU device not found')
-
-# os.startfile("C:\\Users\\User\\Documents\\GitHub\\gnome-scripts\\auto-import\\UABE\\64bit\\AssetBundleExtractor.exe")
-# time.sleep(2)
 
 def click_file(clickType="double", file_path="", center=False, duration=0, add_left=0, add_top=0):
     # time.sleep(duration)
@@ -31,40 +14,39 @@ def click_file(clickType="double", file_path="", center=False, duration=0, add_l
 
     return left, top
 
-def find_file_position(file_path,center=False):
+def find_file_position(file_path, center=False):
+    # Load the image and preprocess it
+    img = cv2.imread(file_path)
+
     # Get the coordinates of the file on the screen
     file_location = None
+    grayscale = False
+    confidence = 0.8
     
     max_attempts = 5
     for attempt in range(max_attempts):
         print(f'[Find Location] Attempt {attempt}')
         print(f'Primary File check: {file_path}')
-        # with tf.compat.v1.Session() as sess:
-        #     with tf.device("/device:GPU:0"):
-        # Your code here
-        file_location = pyautogui.locateOnScreen(file_path, grayscale=True)
+
+        file_location = pyautogui.locateOnScreen(img, grayscale=grayscale, confidence=confidence)
         filename, file_extension = os.path.splitext(file_path)
-        if file_location == None and os.path.exists("imagelocate/"+os.path.basename(filename)+str(2)+file_extension):
+        if file_location is None and os.path.exists("imagelocate/"+os.path.basename(filename)+str(2)+file_extension):
             print(f'Secondary File check: {"imagelocate/"+os.path.basename(filename)+str(2)+file_extension}')
-            # with tf.compat.v1.Session() as sess:
-            #     with tf.device("/device:GPU:0"):
-            #         # Your code here
-            file_location = pyautogui.locateOnScreen("imagelocate/"+os.path.basename(filename)+str(2)+file_extension, grayscale=True)
+            img2 = cv2.imread("imagelocate/"+os.path.basename(filename)+str(2)+file_extension)
+            file_location = pyautogui.locateOnScreen(img2, grayscale=grayscale, confidence=confidence)
             pass
 
         print(file_location)
         if file_location is not None:
             break
 
-    if center == True:
+    if center:
         file_location = pyautogui.center(file_location)
 
     # Click the file if it is found
     if file_location is not None:
-        # print(file_location)
-
         # Extract the left and top values from the location tuple
-        if center == True:
+        if center:
             left, top = file_location.x, file_location.y
         else:
             left, top = file_location.left, file_location.top
@@ -82,9 +64,6 @@ def find_file_position(file_path,center=False):
 folder = "asset_files"
 asset_xml = "assetslocation/apk/textassets.xml"
 
-# with tf.compat.v1.Session() as sess:
-#     with tf.device("/device:GPU:0"):
-#         # Your code here
 #loop over the files in the directory
 for filename in os.listdir(folder):
     left, top = click_file(clickType="double", file_path="imagelocate/UABE.png", duration=0, add_left=-10, add_top=35)
@@ -135,11 +114,11 @@ for filename in os.listdir(folder):
     pyautogui.typewrite(f'{filename}')
     pyautogui.press('enter')
 
-    click_file(clickType="double", file_path="imagelocate/OK.png", center=True)
+    click_file(clickType="single", file_path="imagelocate/OK.png", center=True)
     pyautogui.press('enter')
 
     print(f"[SAVE] Saving new assetbundle {filename}\n\n")
-    click_file(clickType="double", file_path="imagelocate/RESULTS.png", center=True)
+    click_file(clickType="single", file_path="imagelocate/RESULTS.png", center=True)
     pyautogui.press('tab')
     pyautogui.press('tab')
     pyautogui.press('tab')
