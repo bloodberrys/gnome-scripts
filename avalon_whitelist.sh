a3d1d9b05ab4f94ae7b623b0ad8e14abda3ce8d5
bloodberrys <105913725+gnomefin@users.noreply.github.com>
2023-06-25 23:28:15 +0700

    avalon whitelist

---
+++ avalon_whitelist.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# IP address to check and delete
+ip_address="$1"
+
+# Check if the IP address is present in iptables
+if iptables -S | grep -q "$ip_address"; then
+  # Delete the IP address from iptables
+  iptables -D INPUT -s "$ip_address/32" -j ACCEPT
+  echo "IP address $ip_address deleted from iptables."
+  echo $ip_address >> avalon_whitelisted_ips.txt
+  service iptables save
+else
+  echo "IP address $ip_address not found in iptables."
+fi
