4a8aa6bafbaf499affeacf68854933956402731c
bloodberrys <105913725+gnomefin@users.noreply.github.com>
2023-06-25 13:14:18 +0700

    avalon

---
+++ avalon_ddos_blocker.sh
@@ -0,0 +1,143 @@
+#!/bin/bash
+
+run_process(){
+    local _filename=$1
+    local _timestamp=$2
+    array_of_ip_count=$(cat /tmp/${_filename}.log | grep -Eo '^[0-9]+' | tr '\n' ' ')
+    array_of_ip=$(cat /tmp/${_filename}.log | grep -Eo '[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}$' | tr '\n' ' ')
+
+    IFS=" " read -r -a ipcounts <<< "$array_of_ip_count"
+    IFS=" " read -r -a ips <<< "$array_of_ip"
+
+    IFS=" " read -r -a whitelisted_ip <<< "114.124.144.26 49.144.38.198 114.142.173.62 120.28.216.173 52.219.128.250 52.219.132.90 52.219.36.66 45.202.25.237 52.219.32.246 52.219.132.186 52.219.124.50 49.144.43.24 112.215.245.216 112.215.200.69 110.54.184.23 175.176.20.22 120.28.216.28 175.176.32.24 46.146.160.25"
+
+    dir="/tmp/ip_blocked"
+    dir2="/tmp/iplist"
+
+    if [[ ! -e $dir ]]; then
+        sudo mkdir -p $dir
+    elif [[ ! -d $dir ]]; then
+        echo "$dir already exists but is not a directory" 1>&2
+    fi
+    if [[ ! -e $dir2 ]]; then
+        sudo mkdir -p $dir2
+    elif [[ ! -d $dir2 ]]; then
+        echo "$dir2 already exists but is not a directory" 1>&2
+    fi
+
+    iplength=${#ips[@]}
+    counter=0
+    for((i=0; i<iplength; i++))
+    do  
+        whitelist=0
+        # whitelist check
+        if [[ " ${whitelisted_ip[*]} " =~ " ${ips[$i]} " ]]; then
+            # whatever you want to do when array contains value
+            whitelist=1
+        fi
+
+        # check the ip count that more than 7
+        if [ ${ipcounts[$i]} -gt 9 ] && [ ${whitelist} -eq 0 ]; then
+            
+            # checking the ips
+            sudo iptables -C INPUT -s ${ips[$i]} -j DROP
+
+            # If the exit status is 1, meaning we need to execute the ips to ip tables
+            if [ $? -eq 1 ]; then
+                # save the ip to list with command
+                echo "iptables -A INPUT -s ${ips[$i]} -j DROP" >> "/tmp/ip_blocked/ip_blocked_${_filename}.log"
+                echo "${ips[$i]}" >> "/tmp/iplist/iplist_${_filename}.log"
+
+                # execute the iptables block
+                sudo iptables -A INPUT -s ${ips[$i]} -j DROP
+                counter=$((counter+1))
+            fi
+        fi
+    done
+
+    if [ $counter -gt 0 ]; then
+        # persistent save iptables
+        service iptables save       
+    else
+        echo -e "ALL GOOD, no threat detected, ALL OK and nothing to do."
+    fi
+
+    # always execute the report each run_process call
+    IP_IPLIST_LOG_FILENAME=$(find /tmp/iplist/ -type f -printf "%f\n" -mmin +1 | head -n 1)
+    if [ -z "$IP_IPLIST_LOG_FILENAME" ]; then
+        echo "ALL GOOD, no iplist file detected, nothing to do"
+    else
+        message=$(awk '{printf "%s<br>", $0}' "/tmp/iplist/${IP_IPLIST_LOG_FILENAME}")
+        string="There are $counter IPs blocked:<br>$message"
+        send_discord_security_report "$string" "$_timestamp"
+    fi 
+    counter=0
+
+}
+
+send_discord_security_report(){
+    webhook_url=https://discord.com/api/webhooks/1122260171972952104/Pu8LhyV7Yf7G1q-KYzmcWLKhAzefcrCvz88R9XXiIlKMyDAc2BJmQl_s2BtR-KbZaAwJ
+    local _message=$1
+    local _timestamp=$2
+
+    # count total files in a directory
+    total_files=$(ls /tmp/ip_blocked/ | wc -l)
+    total_files2=$(ls /tmp/iplist/ | wc -l)
+
+    if [ ${total_files} -eq 0 ] && [ ${total_files2} -eq 0 ]; then
+        echo "nothing to send as security notif"
+    
+    else
+        IP_BLOCKED_TO_BE_SENT=$(find /tmp/ip_blocked/ -type f -printf "%f\n" -mmin +1 | head -n 1)
+        IP_IPLIST_TO_BE_SENT=$(find /tmp/iplist/ -type f -printf "%f\n" -mmin +1 | head -n 1)
+
+        prefix_1=/tmp/ip_blocked/
+        prefix_2=/tmp/iplist/
+
+        IP_BLOCKED=$(echo $prefix_1$IP_BLOCKED_TO_BE_SENT)
+        IP_IPLIST=$(echo $prefix_2$IP_IPLIST_TO_BE_SENT)
+
+        SUBJECT="⚠️ Security Report - $_timestamp ⚠️"
+        CONTENT=$(echo $_message | sed 's3<br>3\n3g')
+        size=${#CONTENT}
+        if [[ $size -gt 2000 ]]; then
+            CONTENT=${CONTENT:0:1500}
+        fi
+        payload_json=$(jq -n --arg content "$CONTENT" --arg subject "$SUBJECT" '{username: "Gnome-Security", content: "\( $subject )\n\n\( $content )"}')
+        curl -g -F "payload_json=$payload_json" -F "file1=@$IP_BLOCKED" -F "file2=@$IP_IPLIST" "$webhook_url"
+
+        sudo rm -rf $IP_BLOCKED
+        sudo rm -rf $IP_IPLIST
+    fi
+
+}
+
+export TZ=Asia/Jakarta
+timestamp=$(date '+%Y-%m-%d_%H-%M-%S_%Z')
+
+echo -e "[TASK] Checking the netstat..."
+netstat -npt | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr | grep -Eo '[0-9]+[ ]{1}[0-9.]+' | grep -vE '^[0-9]+[ ][1][0].[0].[0-9]{1,3}.[0-9]{1,3}$' | grep -vE '^[0-9]+[ ]127.0.0.1$' > "/tmp/ip_list_total_$timestamp.log"
+netstat -npt | grep 13412 | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr | grep -Eo '[0-9]+[ ]{1}[0-9.]+' | grep -vE '^[0-9]+[ ][1][0].[0].[0-9]{1,3}.[0-9]{1,3}$' | grep -vE '^[0-9]+[ ]127.0.0.1$' > "/tmp/ip_13412_$timestamp.log"
+
+echo -e "\n[PRE-TASK] Obtaining and processing the netstat result file from /tmp/ip_list_total.log and /tmp/ip_13412.log"
+ipfiles="/tmp/ip_list_total_$timestamp.log" # WE CAN SIMPLY CHANGE THIS FOR DEBUGGING
+if [ ! -f "$ipfiles" ]; then
+    echo "✗ File ${ipfiles} not found on your machine!"
+    echo "exitting..."
+    exit 1
+else
+    echo "✓ File ${ipfiles} is found, continue to the next task..."
+    ipfiles2="/tmp/ip_13412_$timestamp.log" # WE CAN SIMPLY CHANGE THIS FOR DEBUGGING
+    if [ ! -f "$ipfiles2" ]; then
+        echo "✗ File ${ipfiles2} not found on your machine!"
+        echo "exitting..."
+        exit 1
+    else
+        echo "✓ File ${ipfiles2} is found, continue to the next task..."
+        run_process "ip_list_total_$timestamp" "$timestamp"
+        run_process "ip_13412_$timestamp" "$timestamp"
+    fi
+fi
+
+
+
---
+++ avalon_healthcheck.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+get_log(){
+	local _server=$1
+    local _base_dir_log=/data/s1001/bin/log
+
+    echo "$_server"
+
+    # get recent log file
+    go=$(ls ${_base_dir_log} -tr | grep -E "${_server}[-_0-9\.log]+$" | tail -n 1)
+    local _recent_log_file=$go
+
+    logs=$(tail -n 100 $_base_dir_log/$_recent_log_file)
+
+    echo $logs
+}
+
+send_mail(){
+    domain=verification.gnome-hub.com
+    api_key=--
+    subject="[AVALON] Server Healthcheck Down Detected"
+    local _message=$1
+    to="alfianvansykes@gmail.com"
+    from='Gnome Automation <automation@verification.gnome-hub.com>'
+    url="https://api.mailgun.net/v3/$domain/messages"
+    req="curl -g --user 'api:${api_key}' '$url' -F from='${from}' -F to='${to}' -F subject='${subject}' -F html=\"${message}\""
+    eval "$req"
+}
+
+send_discord(){
+    webhook_url=https://discord.com/api/webhooks/1122257679704932492/jRwpX8c9pHCbEtdDoNq2UnVE-BBf0Das7Pz0zP_3NQP8tTjYPbrYyVLN2gfvW_ouaKw-
+    SUBJECT="!!![AVALON] Server Healthcheck: Down Detected!!!"
+    local _message=$1
+    ADMIN_ROLE="<@&983558291261112370>"
+    BOOSTER_ROLE="<@&983549809409552445>"
+    CONTENT=$(echo $message | sed 's3<br>3\n3g')
+    size=${#CONTENT}
+    echo -e "$CONTENT" > /tmp/discordmsg.log
+    if [[ $size -gt 2000 ]]; then
+        CONTENT=${CONTENT:0:1500}
+    fi
+    payload_json=$(jq -n --arg content "$CONTENT" --arg subject "$SUBJECT" --arg ar "$ADMIN_ROLE" --arg br "$BOOSTER_ROLE" '{username: "Avalon-Automation", content: "\( $subject )\nCC: \( $br ) \( $ar )\n\n\( $content )"}')
+    curl -g -F "payload_json=$payload_json" -F "file1=@/tmp/discordmsg.log" "$webhook_url"
+}
+
+send_discord_server_up(){
+    webhook_url=https://discord.com/api/webhooks/1122257679704932492/jRwpX8c9pHCbEtdDoNq2UnVE-BBf0Das7Pz0zP_3NQP8tTjYPbrYyVLN2gfvW_ouaKw-
+    SUBJECT="[AVALON] Server Online Status"
+    local _message=$1
+    CONTENT=$(echo $_message | sed 's3<br>3\n3g')
+    netstat -s > /tmp/tcpudp.log
+    netstat -npt | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr > /tmp/tcp_by_ip_count.log
+    netstat -npt | grep 13412 | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr > /tmp/tcp_13412.log
+    netstat -npt | grep 25001 | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr > /tmp/tcp_25001.log
+    netstat -npt | grep 24001 | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr > /tmp/tcp_24001.log
+    netstat -npt | awk '{print $6}' | sort | uniq -c | sort -nr > /tmp/synflooddetection.log
+    netstat -npt | grep SYN_RECV | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr > /tmp/singe_ip_attack.log
+    netstat -npt  | grep SYN_RECV | awk '{print $5}' | grep -Eo '([0-9]{1,3}\.){3}[0-9]{1,3}' | cut -d: -f1 | sort | uniq -c | sort -nr > /tmp/multiple_ip_attack.log
+    size=${#CONTENT}
+    payload_json=$(jq -n --arg content "$CONTENT" --arg subject "$SUBJECT" '{username: "Gnome-Automation", content: "\( $subject )\n\n\( $content )"}')
+    curl -g -F "payload_json=$payload_json" -F "file1=@/tmp/tcpudp.log" -F "file2=@/tmp/tcp_by_ip_count.log" -F "file3=@/tmp/tcp_13412.log" -F "file4=@/tmp/tcp_24001.log" -F "file5=@/tmp/tcp_25001.log" -F "file6=@/tmp/synflooddetection.log" -F "file7=@/tmp/singe_ip_attack.log" -F "file8=@/tmp/multiple_ip_attack.log" "$webhook_url"
+}
+
+lcomma() { 
+    sed '$x;$G;/\(.*\),/!H;//!{$!d};$!x;$s//\1/;s/^\n//'
+}
+
+check_counter() {
+    FILE=/tmp/counter_dn/counter
+    if test -f "$FILE"; then
+        string=$(cat /tmp/counter_dn/counter)
+        size=${#string}
+        if [ ${size} -gt 2 ]; then
+            echo "already 3 times, skipping..."
+            deletefile=$(find /tmp/counter_dn/* -maxdepth 1 -mmin +10 -type f)
+            if [[ -n "$deletefile" ]]; then
+                rm -rf /tmp/counter_dn/
+            fi
+            exit 1;
+        fi
+        echo -n 1 >> /tmp/counter_dn/counter
+    else
+        mkdir -p /tmp/counter_dn
+        touch /tmp/counter_dn/counter
+        echo -n 1 >> /tmp/counter_dn/counter
+    fi
+}
+
+echo "get server-list..."
+IFS=" " read -r -a SERVER_LISTS <<< "centerserver loginserver versionserver idipserver worldserver teamserver routerserver dbserver gameserver gateserver masterserver controlserver"
+
+iteration=0
+down_count=0
+up_count=0
+serverlength=${#SERVER_LISTS[@]}
+string_logs=''
+string_server=''
+for((i=0; i<serverlength; i++))
+do
+    echo "Checking ${SERVER_LISTS[$i]}..."
+    cmd1="ps -Ao pid= -o comm= "
+    cmd2="grep ${SERVER_LISTS[$i]}"
+    is_server_running=$(eval "$cmd1" | eval "$cmd2")
+    if [ -z "${is_server_running}" ]; then
+        # get log and send alert via email
+        timestamp=$(date '+%Y-%m-%d %H:%M:%S %Z')
+        echo -e "!!! CAUGHT SERVER DOWN: ${SERVER_LISTS[$i]}\n"
+        string_server+="${SERVER_LISTS[$i]}, "
+        cmd3="get_log ${SERVER_LISTS[$i]}"
+        logs=$(eval "$cmd3")
+        string_logs+="$timestamp || ${SERVER_LISTS[$i]}=======<br><br>$logs<br><br>"
+        down_count=$((down_count+1))
+    else
+        echo -e "Server ${SERVER_LISTS[$i]} OK!\n"
+        up_count=$((up_count+1))
+    fi
+    iteration=$((iteration+1))
+    sleep 1
+done
+
+echo -e "[AVALON] All checked servers: $iteration"
+echo -e "[AVALON] Server down: $down_count\nServer up: $up_count\n"
+stats="[AVALON] Server down: $down_count<br>Server up: $up_count<br>"
+string_server=$(echo "$string_server" | lcomma)
+server_affected=$string_server
+
+
+if [ -z "${server_affected}" ]; then
+    # nothing to do
+    tcp_connection_count=$(netstat -an | grep -c ESTABLISHED)
+    # online_player_count=$(curl 127.0.0.1:81/sdk/healthcheck.php | jq -r .online_role)
+    export TZ=Asia/Jakarta
+    date=$(date '+%Y-%m-%d_%H-%M-%S_%Z')
+    messages_up="==================================<br>SERVER UP AND RUNNING ${date}:white_check_mark:<br>==================================<br>tcp connection: ${tcp_connection_count}<br>player online: ${online_player_count}<br>==================================<br>"
+    send_discord_server_up "$messages_up"
+    # echo -e "online: ${online_player_count}"
+    echo -e "online: ${tcp_connection_count}"
+    echo -e "server up!!!"
+    exit 0;
+else
+    # if server affcted > 0, send email
+    check_counter
+    message="=====Stats=====<br><br>$stats<br><br>Server down lists: $server_affected<br><br>======LOGS=======<br><br>$string_logs"
+    send_discord "$message"
+    # send_mail "$message"
+    exit 0;
+fi
+
+
+
